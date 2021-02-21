eb54531fa34268482a00f1c399e2d7d2f8ac7bc9
rose <vcdaquiado@gmail.com>
2021-02-21 12:06:50 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PersonalWebsite
+A simple website, where I can showcase my skills and projects
---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+
+
+const op = setInterval(increaseNum, 20);
+
+function increaseNum(){
+
+	const text = document.getElementById("text");
+	const progress =  document.getElementById("progress");
+
+	let a = window.getComputedStyle(progress, ':before').getPropertyValue('width');
+
+	a = Math.floor((parseInt(a)/ 10) * 3 +2);
+	text.innerHTML = a + '%';
+	console.log(a);
+	if(a == 100){
+		clearInterval(op);
+
+		$(".loader .inner-text").fadeOut(750, function(){
+			$(".loader").fadeOut(800,function(){
+				$(".loader .main ").fadeOut(1000)
+			});
+	  });
+	}
+
+
+}
+
